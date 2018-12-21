48563727404ac308abf257ecd66041accc6fa946
Nic <Wanavit>
2018-12-21 22:50:37 +0800

    test train model without external function add epochs inarg print every

--- train.py
+++ train.py
@@ -144,6 +144,7 @@ t0=time()
 ## test deep learning without external function
 # method for validation
 epochs=in_arg.epochs
+print_every=50
 def validation(model, validloader, criterion):
     valid_loss = 0
     accuracy = 0
