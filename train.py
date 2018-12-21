9d3e631e77b39a919532c4afa59a89f98cba200e
Nic <Wanavit>
2018-12-21 22:46:50 +0800

    test train model without external function

--- train.py
+++ train.py
@@ -139,7 +139,72 @@ device='cpu'if in_arg.gpu==False else 'gpu'
 #### Training
 print('training with ',device)
 t0=time()
-model = do_deep_learning(model, trainloader, in_arg.epochs, 40, criterion, optimizer,validloader, device)
+#model = do_deep_learning(model, trainloader, in_arg.epochs, 40, criterion, optimizer,validloader, device)
+
+## test deep learning without external function
+# method for validation
+def validation(model, validloader, criterion):
+    valid_loss = 0
+    accuracy = 0
+    for images, labels in validloader:
+
+        images, labels = images.to('cuda'), labels.to('cuda')
+
+        output = model.forward(images)
+        valid_loss += criterion(output, labels).item()
+
+        ps = torch.exp(output)
+        equality = (labels.data == ps.max(dim=1)[1])
+        accuracy += equality.type(torch.FloatTensor).mean()
+
+    return valid_loss, accuracy
+
+## Train the network
+steps=0
+model.to('cuda')
+for e in range(epochs):
+    running_loss = 0
+    model.train()
+    for images, labels in trainloader:
+        steps += 1
+
+        images, labels = images.to('cuda'), labels.to('cuda')
+
+        optimizer.zero_grad()
+
+        # Forward and backward passes
+        outputs = model.forward(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        if steps % print_every == 0:
+            # change nn to eval mode for inference
+            model.eval()
+
+            # turn off gradients for validation
+            with torch.no_grad():
+                valid_loss, valid_accuracy = validation(model, validloader, criterion)
+
+            print("Epoch: {}/{}... ".format(e+1, epochs),
+                  "Train Loss: {:.4f}".format(running_loss/print_every),
+                  "Validation Loss: {:.4f}".format(valid_loss/len(validloader)),
+                  "Validation Accuracy: {:.4f}".format(valid_accuracy/len(validloader)))
+
+            running_loss = 0
+
+
+
+
+
+
+
+
+
+
+
 print('the model took',time()-t0,'seconds to train')
 pp.pprint(model.__dict__)
 
